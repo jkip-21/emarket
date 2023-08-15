6670a4cf7543d3e5217ad98760f75c08b76191ba
Jonah Kiptoo <jonnahkapkory15384@gmail.com>
2023-08-15 14:13:20 +0300

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   const {products} = useContext(ProductContext);
   console.log(products);
 
-  // filter products
+  // filter all products
 
   const filteredProducts = products.filter(item => {
     return item.category === "men's clothing" || item.category === "women's clothing"
